b286ef734f5bd5aec7558e14c5da735d03a5a3bb
Lotusxx <the_end_song@festa.ocn.ne.jp>
2023-04-17 15:42:57 +0900

    list-display
    TODOリスト、リスト表示

---
+++ リスト表示/djangotodo/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView
+from .models import Task
+
+class TaskList(ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+    ordering = ['-created_at']
